09246496595a76ace201ee87408f324b8f76aa3a
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2022-06-04 19:20:40 -0300

    finalizei o loop do html e estilizei a pagina adicionando nuvens

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,30 @@
 const sonic = document.querySelector('.sonic');
+const spike = document.querySelector('.spike');
 const jump = () =>{
     sonic.classList.add('jump')
     setTimeout(()=> {
         sonic.classList.remove('jump')
     },700);
 }
+
+const loop = setInterval(()=>{
+  console.log(loop);
+    const spikePosition = spike.offsetLeft;
+    const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+    //o + converte o string para number
+   
+    if(spikePosition <= 93 && spikePosition > 0 && sonicPosition < 50 ){
+        spike.style.animation = 'none';
+        spike.style.left = `${spikePosition}px`;
+        sonic.style.animation = 'none';
+        sonic.style.bottom = `${sonicPosition}px`;
+
+        sonic.src = "./assets/s-game-over.png"
+        
+        clearInterval(loop)
+        //o clearInterval faz esse loop para quando eu perco
+    
+    }
+},10)
 
 document.addEventListener('keydown', jump)
